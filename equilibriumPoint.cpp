4ec2de32de0ae05fb1ddb88e1e4fe42ca1a1452d
Gonthini Bhaskar <49478324+bhaskargonthini@users.noreply.github.com>
2022-07-16 10:38:13 +0530

    Add files via upload

---
+++ equilibriumPoint.cpp
@@ -0,0 +1,53 @@
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        // Your code here
+        int sum = 0, lsum = 0;
+        for(int i=0;i<n;i++) sum+= a[i];
+        for(int i=0;i<n;i++){
+            if(lsum == sum-a[i]) return i+1;
+            lsum+= a[i];
+            sum-= a[i];
+        }
+        return -1;
+    }
+
+};
+
+// { Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
