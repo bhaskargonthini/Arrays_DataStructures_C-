289ccc96bbe6302121da842efcd0a429e9ef693d
Gonthini Bhaskar <49478324+bhaskargonthini@users.noreply.github.com>
2022-07-16 10:39:02 +0530

    Add files via upload

---
+++ frequency_count.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+        // code here
+        vector<int> v(N+1,0);
+        for(int i=0;i<N;i++){
+            if(arr[i]<=N && arr[i]>0){
+                v[arr[i]]++;
+            }
+        }
+        for(int i=1;i<N+1;i++){
+            arr[i-1] = v[i];
+        }
+        
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	long long t;
+	
+	//testcases
+	cin >> t;
+	
+	while(t--){
+	    
+	    int N, P;
+	    //size of array
+	    cin >> N; 
+	    
+	    vector<int> arr(N);
+	    
+	    //adding elements to the vector
+	    for(int i = 0; i < N ; i++){
+	        cin >> arr[i]; 
+	    }
+        cin >> P;
+        Solution ob;
+        //calling frequncycount() function
+		ob.frequencyCount(arr, N, P); 
+		
+		//printing array elements
+	    for (int i = 0; i < N ; i++) 
+			cout << arr[i] << " ";
+	    cout << endl;
+	}	
+	return 0; 
+}
+
+
+
+
+  // } Driver Code Ends
