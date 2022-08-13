59f030cc9a81f58eb868344d05f1e8ab28a286e9
Gonthini Bhaskar <49478324+bhaskargonthini@users.noreply.github.com>
2022-08-13 12:37:58 +0530

    Create arrays_sortingSTL_inC++.cpp

---
+++ arrays_sortingSTL_inC++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	
+	int arr[]={10,20,5,7};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	
+	sort(arr,arr+n);
+	
+	for(int x: arr)
+	    cout<<x<<" ";
+	
+	sort(arr,arr+n,greater<int>());
+	
+	cout<<endl;
+	for(int x: arr)
+	    cout<<x<<" ";
+}
