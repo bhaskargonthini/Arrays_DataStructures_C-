45b1d42e6cbcd66e84a7d8416e5ff9e6c05a02bc
Gonthini Bhaskar <49478324+bhaskargonthini@users.noreply.github.com>
2022-09-05 09:29:57 +0530

    Update equilibriumPoint.cpp

--- equilibriumPoint.cpp
+++ equilibriumPoint.cpp
@@ -1,13 +1,13 @@
 
 
- // } Driver Code Ends
+ // } 
 class Solution{
     public:
     // Function to find equilibrium point in the array.
     // a: input array
     // n: size of array
     int equilibriumPoint(long long a[], int n) {
-    
+  
         int sum = 0, lsum = 0;
         for(int i=0;i<n;i++) sum+= a[i];
         for(int i=0;i<n;i++){
